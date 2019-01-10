fae17e5400aa6c243207fb299a001032c12658a4
tiltowait <tiltowaitt+github@icloud.com>
2019-01-09 23:14:52 -0800

    Remove "Kaleidoscope-" prefix to enhance portability for testing purposes

--- src/Kaleidoscope/ProperShifting.cpp
+++ src/Kaleidoscope/ProperShifting.cpp
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Kaleidoscope-ProperShifting.h"
+#include "ProperShifting.h"
 
 namespace kaleidoscope {
 
